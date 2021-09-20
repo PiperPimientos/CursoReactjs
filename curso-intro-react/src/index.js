6749dd0289ab5d14d25669a31ab88db0627868c5
felipe <feliperestrepomeneses@gmail.com>
2021-09-19 23:37:36 -0500

    modificamos nuestros archivos index.js y App.js para primero caer en cuenta que App.js es un componente de React, que contiene propiedades y que estas propiedades son invisibles ppara html, pero igual se ejecutan. Mirar las notas para profundizar mas

--- curso-intro-react/src/index.js
+++ curso-intro-react/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App>
+    <h1>Esto es un titulo</h1>
+  </App>,
   document.getElementById('root')
 );
 
