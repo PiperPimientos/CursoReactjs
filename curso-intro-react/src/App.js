ec925ad8428a3dfca344af1ad5f081b1823b3baa
felipe <feliperestrepomeneses@gmail.com>
2021-09-21 23:27:54 -0500

    Agregamos los archivos de los componentes que ya estan estructurados en CreateTodoButton.js, TodoCounter.js, TodoItem.js,TodoList.js, TodoSearch.js, esta es la v.1.2

--- curso-intro-react/src/App.js
+++ curso-intro-react/src/App.js
@@ -1,14 +1,31 @@
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoItem } from "./TodoItem";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+
 // import './App.css';
+
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso de intro a react', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+]
 
 function App(props) {
   return (
-    <TodoCounter />,
-    <TodoSearch />,
-    <TodoList>
-      <TodoItem />,
-    </TodoList>,
-    <CreateTodoButton />,
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}
+        </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
-}
+};
 
 export default App;
